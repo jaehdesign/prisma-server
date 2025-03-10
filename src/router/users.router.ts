daeb74fc9d94fed4d4b1923d791320a8ea5761fb
juanan <jaehdesign@gmailcom>
2025-03-10 11:09:44 +0100

    updateUpdate prisma-server reviews clase 070325 v3

--- src/router/users.router.ts
+++ src/router/users.router.ts
@@ -6,11 +6,11 @@ const debug = createDebug('movies:router:users');
 export const createUsersRouter = (usersController: UsersController) => {
     debug('Ejecutando createFilmsRouter');
     const usersRouter = Router();
-    //usersRouter.get('/', usersController.getAll);
-    //usersRouter.get('/:id', usersController.getById);
+    usersRouter.get('/', usersController.getAll.bind(usersController));
+    usersRouter.get('/:id', usersController.getById.bind(usersController));
     usersRouter.post('/register', usersController.create.bind(usersController));
     usersRouter.post('/login', usersController.login.bind(usersController));
-    //usersRouter.patch('/:id', usersController.update);
-    //usersRouter.delete('/:id', usersController.delete);
+    usersRouter.patch('/:id', usersController.update.bind(usersController));
+    usersRouter.delete('/:id', usersController.delete.bind(usersController));
     return usersRouter;
 };
